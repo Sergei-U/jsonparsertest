0b43961993a2ce3da3792b81b763600eeac4884b
swizz <swizze@bk.ru>
2021-06-07 14:41:26 +0300

    4

--- src/main/java/JsonTicketParser.java
+++ src/main/java/JsonTicketParser.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -21,26 +22,47 @@ public class JsonTicketParser {
             DesTickets desTickets = objectMapper.readValue(new File("src/main/resources/tickets.json"), DesTickets.class);
             objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
-            List<Duration> durationList = new ArrayList<>();
-            System.out.println(desTickets.getTickets());
-            Duration resultD = null;
+            List<String> durationList = new ArrayList<>();
+            List<Long> durationListSeconds = new ArrayList<>();
+            Long durationTime = 0L;
+            Long timeTravel = 0L;
+
             for (int i = 0; i < desTickets.getTickets().size(); i++) {
 
                 LocalDateTime start = LocalDateTime.parse(desTickets.getTickets().get(i).getDeparture_date() + " " + desTickets.getTickets().get(i).getDeparture_time(), formatter);
                 LocalDateTime end = LocalDateTime.parse(desTickets.getTickets().get(i).getArrival_date() + " " + desTickets.getTickets().get(i).getArrival_time(), formatter);
                 Duration duration = Duration.between(end, start).abs().plusHours(7);
 
-                System.out.printf(
-                        "%dд %dч %dмин%n",
-                        duration.toDays(),
-                        duration.toHours() % 24,
-                        duration.toMinutes() % 60
+                timeTravel = duration.toSeconds();
+                String str = String.format("%d:%d:%d%n", duration.toDays(), duration.toHours() % 24, duration.toMinutes() % 60);
+
+                durationList.add(str);
+                durationListSeconds.add(timeTravel);
+
+
+            }
+            for(int j=0; j<durationListSeconds.size(); j++) {
+                durationTime += durationListSeconds.get(j);
+            }
+            Duration avgTimeTravel = Duration.ofSeconds(durationTime/durationListSeconds.size());
+            System.out.printf(
+                        "Среднее время полета: %dд %dч %dмин%n",
+                    avgTimeTravel.toDays(),
+                    avgTimeTravel.toHours() % 24,
+                    avgTimeTravel.toMinutes() % 60
                 );
-                durationList.add(duration);
-            }
-            for(int j = 0; j<durationList.size(); j ++) {
-                resultD += Duration.between(durationList.get(j));
-            }
+
+            Collections.sort(durationListSeconds);
+            int index = (int) Math.ceil(90 / 100.0 * durationListSeconds.size());
+            Duration persentileTime = Duration.ofSeconds(durationListSeconds.get(index-1));
+            System.out.printf(
+                    "90 percentile: %dд %dч %dмин%n",
+                    persentileTime.toDays(),
+                    persentileTime.toHours() % 24,
+                    persentileTime.toMinutes() % 60
+            );
+
+
     } catch(
     IOException e)
 
